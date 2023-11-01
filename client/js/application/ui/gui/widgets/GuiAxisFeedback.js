0213c650611ebf804af9c9eb53c560af1fbb270c
Oskar Åsbrink <oskar.asbrink@gmail.com>
2023-11-01 14:38:59 +0100

    color curves for fades

--- client/js/application/ui/gui/widgets/GuiAxisFeedback.js
+++ client/js/application/ui/gui/widgets/GuiAxisFeedback.js
@@ -1,14 +1,42 @@
 import { GuiWidget} from "../elements/GuiWidget.js";
 
 let applyPositionOffset = function(guiAxisSlider) {
-    guiAxisSlider.guiWidget.offsetWidgetPosition(guiAxisSlider.offset);
+    let guiWidget = guiAxisSlider.guiWidget
+    let options = guiAxisSlider.options
+    guiWidget.offsetWidgetPosition(guiAxisSlider.offset);
     if (guiAxisSlider.rotation) {
-        guiAxisSlider.guiWidget.setWidgetRotation(guiAxisSlider.rotation)
+        guiWidget.setWidgetRotation(guiAxisSlider.rotation)
     }
 
     if (guiAxisSlider.valueText) {
-        guiAxisSlider.guiWidget.text.maxRows = 1;
-        guiAxisSlider.guiWidget.printWidgetText(guiAxisSlider.valueText)
+        guiWidget.text.maxRows = 1;
+        if (guiWidget.text)
+        guiWidget.printWidgetText(guiAxisSlider.valueText)
+    }
+
+    if (options.color_curves[0] || options.color_curves[1]) {
+        let originalRgba = options.color_rgba
+        let rgba = guiAxisSlider.rgba;
+        let rgbaMod = guiAxisSlider.rgbaMod;
+        rgba.r = originalRgba.r * (1 - rgbaMod.r)
+        rgba.g = originalRgba.g * (1 - rgbaMod.g)
+        rgba.b = originalRgba.b * (1 - rgbaMod.b)
+        rgba.a = originalRgba.a * (1 - rgbaMod.a)
+
+
+        let text = guiWidget.text;
+        if (text) {
+            text.setTextColor(rgba)
+        }
+
+        let icon = guiWidget.icon;
+        if (icon) {
+            icon.setGuiIconColorRGBA(rgba)
+        }
+
+        let surface = guiWidget.guiSurface;
+        surface.getBufferElement().setColorRGBA(rgba)
+
     }
 
 };
@@ -35,6 +63,29 @@ let onFrameUpdate = function(guiAxisSlider, tpf, time) {
     if (options.value_text[1] !== null) {
         text += MATH.numberToDigits(yValue, options.value_text[1], options.value_text[1])
     }
+
+    let rgbaMod = guiAxisSlider.rgbaMod;
+    rgbaMod.r = 0;
+    rgbaMod.g = 0;
+    rgbaMod.b = 0;
+    rgbaMod.a = 0;
+
+    if (options.color_curves[0]) {
+        let curveValue = MATH.valueFromCurve(xValue, MATH.curves[options.color_curves[0]])
+        rgbaMod.r += options.curve_colors[0][0]*curveValue;
+        rgbaMod.g += options.curve_colors[0][1]*curveValue;
+        rgbaMod.b += options.curve_colors[0][2]*curveValue;
+        rgbaMod.a += options.curve_colors[0][3]*curveValue;
+    }
+
+    if (options.color_curves[1]) {
+        let curveValue = MATH.valueFromCurve(yValue, MATH.curves[options.color_curves[1]])
+        rgbaMod.r += options.curve_colors[1][0]*curveValue;
+        rgbaMod.g += options.curve_colors[1][1]*curveValue;
+        rgbaMod.b += options.curve_colors[1][2]*curveValue;
+        rgbaMod.a += options.curve_colors[1][3]*curveValue;
+    }
+
     guiAxisSlider.valueText = text;
     applyPositionOffset(guiAxisSlider);
 
@@ -51,7 +102,10 @@ class GuiAxisFeedback {
             "range": [0.08, 0.08],
             "rotation": [0, 0],
             "offsets": [0, 0],
-            "value_text": [null, null]
+            "color_rgba": {r:0.1, g:0.9, b:0.3, a:0.7},
+            "value_text": [null, null],
+            "color_curves": [null, null],
+            "curve_colors": [[1, 1, 1, 1], [1, 1, 1, 1]]
         };
         for (let key in options) {
             this.options[key] = options[key];
@@ -60,6 +114,10 @@ class GuiAxisFeedback {
         this.pos = new THREE.Vector3();
         this.origin = new THREE.Vector3();
         this.offset = new THREE.Vector3();
+
+        this.rgbaMod = {r:0, g:0, b:0, a:0};
+        this.rgba = {r:0, g:0, b:0, a:0};
+
 
         this.rotation = 0;
 
