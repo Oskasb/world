36b2a33f8702950ea6855fdf57965be3fe45270c
oskar <oskar.asbrink@gmail.com>
2024-03-01 10:11:36 +0100

    map stuff

--- client/js/application/ui/dom/DomWorldmap.js
+++ client/js/application/ui/dom/DomWorldmap.js
@@ -1,5 +1,8 @@
 import {HtmlElement} from "./HtmlElement.js";
+import {poolFetch, poolReturn} from "../../utils/PoolUtils.js";
+import {Object3D} from "../../../../libs/three/core/Object3D.js";
 let worldSize = 2048;
+let tempObj = new Object3D();
 function calcMapBackgroundOffset(zoom, axisCenter, worldSize) {
     let zoomOffset = 1 + (1 / zoom);
     return MATH.percentify(zoomOffset*MATH.decimalify(axisCenter, 5)+worldSize*0.5, worldSize, true);
@@ -13,10 +16,21 @@ function calcZoomForSize(size, worldSize)  {
     return worldSize/size;
 }
 
+function positionDiv(div, posVec, zoomFactor) {
+    let mapPctX = posVec.x*zoomFactor
+    let mapPctZ = posVec.z*zoomFactor
+
+    div.style.top = 50 + mapPctZ + '%';
+    div.style.left = 50 + mapPctX + '%';
+}
+
 class DomWorldmap {
     constructor(closeCb) {
         let htmlElement = new HtmlElement();
-
+        let transition = null;
+        let mapDiv = null;
+        let cursorDiv = null;
+        let cameraDiv = null;
         let statusMap = {
             posX : 0,
             posZ : 0,
@@ -43,8 +57,6 @@ class DomWorldmap {
             statusMap.zoom = Math.round(MATH.clamp(statusMap.zoom * 0.5, 1, 32));
         }
 
-        let mapDiv = null;
-        let cursorDiv = null;
 
         let elemECoords = function(e){
             let elem = e.target;
@@ -71,9 +83,29 @@ class DomWorldmap {
   */
         }
 
+        let onArrive = function(endPos, spatTrans) {
+            poolReturn(spatTrans);
+            let selectedActor = GameAPI.getGamePieceSystem().selectedActor;
+            if (selectedActor) {
+                selectedActor.setSpatialPosition(endPos)
+                // selectedActor.transitionTo(endPos, 1);
+            }
+            transition = null;
+        }
+
+
         let mapClick = function(e) {
             elemECoords(e);
             console.log("Map Click", ThreeAPI.tempVec3, e)
+
+            if (transition !== null) {
+                transition.cancelSpatialTransition()
+            }
+
+            transition = poolFetch('SpatialTransition')
+            let distance = MATH.distanceBetween(ThreeAPI.getCameraCursor().getLookAroundPoint(), ThreeAPI.tempVec3)
+            transition.initSpatialTransition(ThreeAPI.getCameraCursor().getLookAroundPoint() , ThreeAPI.tempVec3, 2, onArrive, MATH.curveSqrt(distance*0.4) + distance*0.1)
+        //    ThreeAPI.getCameraCursor().getLookAroundPoint().copy(ThreeAPI.tempVec3);
         //    statusMap['layerX'] = e.layerX;
         //    statusMap['layerY'] = e.layerY;
         //    let elem = e.target;
@@ -91,6 +123,7 @@ class DomWorldmap {
         let readyCb = function() {
             mapDiv = htmlElement.call.getChildElement('worldmap')
             cursorDiv = htmlElement.call.getChildElement('cursor')
+            cameraDiv = htmlElement.call.getChildElement('camera')
             let reloadDiv = htmlElement.call.getChildElement('reload')
 
             let zoomInDiv = htmlElement.call.getChildElement('zoom_in')
@@ -105,11 +138,14 @@ class DomWorldmap {
         let rebuild = htmlElement.initHtmlElement('worldmap', closeMapCb, statusMap, 'full_screen', readyCb);
 
         let update = function() {
-            let cursorPos =  ThreeAPI.getCameraCursor().getPos();
+            let cursorPos =  ThreeAPI.getCameraCursor().getLookAroundPoint();
             statusMap.posX = 'x:'+MATH.decimalify(cursorPos.x, 10);
             statusMap.posZ = 'z:'+MATH.decimalify(cursorPos.z, 10);
 
+
+
             if (mapDiv) {
+                let cam = ThreeAPI.getCamera()
             //    console.log(minimapDiv);
                 let zoom = statusMap.zoom;
                 let zoomFactor = calcMapMeterToDivPercent(zoom, worldSize);
@@ -129,11 +165,16 @@ class DomWorldmap {
                     }
                 }
 
-                let mapPctX = cursorPos.x*zoomFactor
-                let mapPctZ = cursorPos.z*zoomFactor
+                positionDiv(cursorDiv, cursorPos, zoomFactor)
+                positionDiv(cameraDiv, cam.position, zoomFactor)
+            //    tempObj.position.copy(cam.position);
+            //    tempObj.position.y = cursorPos.y;
+            //    tempObj.lookAt(cursorPos)
+            //    let angle = -MATH.eulerFromQuaternion(tempObj.quaternion).y //+ MATH.HALF_PI * 0.5 // Math.PI //;
+            //    cameraDiv.style.rotate = MATH.angleInsideCircle(angle) + 'rad';
 
-                cursorDiv.style.top = 50 + mapPctZ + '%';
-                cursorDiv.style.left = 50 + mapPctX + '%';
+
+
 
             }
 
