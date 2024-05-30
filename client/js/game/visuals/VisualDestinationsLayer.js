b63a4bbad0af9ad9ea32783fc2bb882826ac3d2e
oskar <oskar.asbrink@gmail.com>
2024-05-30 16:12:39 +0200

    show dir to advs

---
+++ client/js/game/visuals/VisualDestinationsLayer.js
@@ -0,0 +1,86 @@
+import {colorMapFx} from "./Colors.js";
+import {getCountClosestFromList} from "../../application/utils/SpatialUtils.js";
+import {poolFetch, poolReturn} from "../../application/utils/PoolUtils.js";
+
+class VisualDestinationsLayer {
+    constructor() {
+
+        let list = null;
+        let countMax = 3;
+        let distanceMax = 200;
+        let fxRgba = colorMapFx['ADVENTURE_HINT']
+
+        let visualisedDestinations = [];
+        let proximateDestinations = [];
+        let activePointers = [];
+
+        function setList(array, rgba, cMax, dMax) {
+            list = array;
+            fxRgba = rgba || colorMapFx['ADVENTURE_HINT']
+            countMax = cMax || countMax;
+            distanceMax = dMax || distanceMax;
+        }
+
+        function clearDestinations() {
+            MATH.emptyArray(visualisedDestinations);
+            MATH.emptyArray(proximateDestinations);
+        }
+
+        function update() {
+            let from = ThreeAPI.getCameraCursor().getPos()
+            if (list.length !== 0) {
+                MATH.emptyArray(visualisedDestinations);
+                getCountClosestFromList(3, list, from, visualisedDestinations)
+            } else {
+                clearDestinations()
+            }
+
+            while (activePointers.length < visualisedDestinations.length) {
+                let pointer = poolFetch('VisualDestinationPointer')
+                pointer.on();
+                activePointers.push(pointer);
+            }
+
+            while (activePointers.length > visualisedDestinations.length) {
+                let pointer = activePointers.pop()
+                pointer.off();
+                poolReturn(pointer);
+            }
+
+            for (let i = 0; i < visualisedDestinations.length; i++) {
+                let pointer = activePointers[i];
+                pointer.from.copy(from);
+                pointer.to.copy(visualisedDestinations[i].getPos())
+                pointer.recalcPoints = true;
+            }
+
+        }
+
+        function close() {
+
+        }
+
+        this.call = {
+            setList:setList,
+            update:update,
+            close:close
+        }
+
+    }
+
+    setDestinations(list, rgba, maxVisible, maxDistance) {
+        this.call.setList(list, rgba, maxVisible, maxDistance)
+    }
+
+    on() {
+        GameAPI.registerGameUpdateCallback(this.call.update)
+    }
+
+    off() {
+        this.call.close();
+        GameAPI.unregisterGameUpdateCallback(this.call.update)
+    }
+
+}
+
+export { VisualDestinationsLayer }
---
+++ client/js/game/visuals/effects/VisualDestinationPointer.js
@@ -0,0 +1,110 @@
+import {Vector3} from "../../../../libs/three/math/Vector3.js";
+import {poolFetch} from "../../../application/utils/PoolUtils.js";
+import {Object3D} from "../../../../libs/three/core/Object3D.js";
+import {colorMapFx} from "../Colors.js";
+import {physicalAlignYGoundTest} from "../../../application/utils/PhysicsUtils.js";
+
+let tempVec = new Vector3();
+let normalStore = new Vector3()
+let tempObj = new Object3D();
+
+class VisualDestinationPointer {
+    constructor() {
+
+        this.from = new Vector3();
+        this.to = new Vector3()
+
+        this.fxPoints = [];
+
+        this.recalcPoints = true;
+
+        let update = function() {
+        //    evt.dispatch(ENUMS.Event.DEBUG_DRAW_LINE, {from:this.from, to:this.to, color:'YELLOW'});
+
+            if (this.recalcPoints === true) {
+
+                while (this.fxPoints.length) {
+                    let pointFX = this.fxPoints.pop();
+                    pointFX.recoverPointFx();
+                }
+                this.recalcPoints = false;
+                tempVec.copy(this.to);
+                tempVec.y = this.from.y;
+            //    console.log("recalcPoints Points",  this.from, this.to, tempVec)
+                let distance = MATH.distanceBetween(this.from, tempVec);
+
+                for (let i = 0; i < distance; i++) {
+                    let pointFX = poolFetch('VisualPointFX')
+                    pointFX.setupPointFX();
+                    this.fxPoints.push(pointFX);
+                }
+
+                tempObj.position.copy(this.from);
+                tempObj.lookAt(tempVec);
+                tempObj.rotateX(-1.57)
+                tempVec.sub(this.from);
+                tempVec.normalize();
+                tempObj.position.x += tempVec.x *0.5;
+                tempObj.position.z += tempVec.z *0.5;
+                let rgba = colorMapFx['GLITTER_FX']
+                //    console.log("Draw Points",  this.fxPoints.length)
+                for (let i = 0; i < this.fxPoints.length; i++) {
+                    let pointFX = this.fxPoints[i];
+                    let fits = physicalAlignYGoundTest(tempObj.position, tempObj.position, 1.5)
+                    if (fits === false) {
+                        tempObj.position.y = 0;
+                        pointFX.updatePointFX(tempObj.position, tempObj.quaternion, rgba)
+                    } else {
+                        tempObj.position.y = Math.max(tempObj.position.y, 0.1);
+                        pointFX.updatePointFX(tempObj.position, tempObj.quaternion, rgba)
+                    }
+                //
+
+                //    evt.dispatch(ENUMS.Event.DEBUG_DRAW_LINE, {from:tempObj.position, to:this.to, color:'YELLOW'});
+                    tempObj.position.add(tempVec);
+                }
+
+            }
+
+
+        }.bind(this)
+
+        this.call = {
+            update:update
+        }
+
+    }
+
+    setFrom(x, z) {
+        if (x !== this.from.x || z !== this.from.z) {
+            this.from.set(x, 0, z);
+            this.from.y = ThreeAPI.terrainAt(this.from);
+            this.recalcPoints = true;
+        }
+    }
+
+    setTo(x, z) {
+        if (x !== this.to.x || z !== this.to.z) {
+            this.to.set(x, 0, z);
+            this.to.y = ThreeAPI.terrainAt(this.to);
+            this.recalcPoints = true;
+        }
+    }
+
+    on() {
+        this.recalcPoints = true;
+        ThreeAPI.addPrerenderCallback(this.call.update);
+    }
+
+
+    off() {
+        while (this.fxPoints.length) {
+            let pointFX = this.fxPoints.pop();
+            pointFX.recoverPointFx();
+        }
+        ThreeAPI.unregisterPrerenderCallback(this.call.update);
+    }
+
+}
+
+export {VisualDestinationPointer}
